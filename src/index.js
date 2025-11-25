0437e681cdb20dc7e02498d1f896a8ef6f1ae937
ahmadayasien-oss <ahmad.a.yasien@gmail.com>
2025-11-25 11:04:03 +0200

    update app file 1

--- src/index.js
+++ src/index.js
@@ -8,6 +8,9 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
