cdabafc9a7336c2ff519cc47c81c97b3e8dc5ce0
ahmadayasien-oss <ahmad.a.yasien@gmail.com>
2025-11-26 13:38:04 +0200

    react-comp

---
+++ src/comp/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <footer>
+    React Course - Level 1 (session 1)
+    <span>🧡</span>
+  </footer>
+        </div>
+    );
+}
+
+export default Footer;
