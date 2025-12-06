8f588a13b0ded34ef2bc51255b6fb66113d56e78
ahmadayasien-oss <ahmad.a.yasien@gmail.com>
2025-12-06 12:15:53 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import {
+
+Routes,Route,
+}from "react-router-dom";
+
+import Home from './pages/home'
+import HTML from './pages/html'
+import Css from './pages/css'
+import Javascript from './pages/javascript'
 
 function App() {
   return (
-    <>
-<header>App File</header>
-</>
+    <div className="App">
+<Routes>
+<Route  path = "/" element ={<Home/>} />
+<Route  path = "/html" element ={<HTML/>} />
+<Route  path = "/css" element ={<Css/>} />
+<Route  path = "/javascript" element ={<Javascript/>} />
+</Routes>
+</div>
 
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,46 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './pages/home'
-import HTML from './pages/html'
-import Css from './pages/css'
-import Javascript from './pages/javascript'
-import {
-createBrowserRouter,
-RouterProvider,
-}from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <h1>SORROY.........</h1>,
-  },
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 
-  {
-    path: "/html",
-    element: <HTML />,
-  },
 
 
-  {
-    path: "/css",
-    element: <Css />,
-  },
-  {
-    path: "/javascript",
-    element: <Javascript />,
-  },
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    
+    <BrowserRouter>
+    <App/>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
